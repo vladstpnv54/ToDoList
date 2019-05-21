0a0046003c487d06f9d833126704870b93f4464d
vladstpnv54 <vladstpnv54@mail.ru>
2019-05-21 19:41:02 +0700

    Create methods for LocalStorage

--- src/script.js
+++ src/script.js
@@ -3,22 +3,32 @@ var taskApp = new Vue({
   data: {
     tasks: []
   },
+  mounted() {
+    if(localStorage.getItem('tasks')) {
+      try {
+        this.tasks = JSON.parse(localStorage.getItem('tasks'));
+      } catch(e) {
+        localStorage.removeItem('tasks');
+      }
+    }
+  },
   methods: {
     addTask: function(e) {
       e.preventDefault();
-      if(this.tasks.name != '' && this.tasks.name != null && this.tasks.name != ' '){
           this.tasks.push({
             name: this.tasks.name,
             done: false
           });
           this.tasks.name = '';
-
-      }
+          this.saveTask();
     },
     deleteTask: function(task) {
       this.tasks.splice(this.tasks.indexOf(task), 1)
-
+      this.saveTask();
     },
-    
+    saveTask() {
+      let parsed = JSON.stringify(this.tasks);
+      localStorage.setItem('tasks', parsed);
+    }
   }
 });
