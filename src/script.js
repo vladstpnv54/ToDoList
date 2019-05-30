8d58199157cc8e5fa90657e103bf3c8adb625b51
vladstpnv54 <vladstpnv54@mail.ru>
2019-05-30 17:44:20 +0700

    Create function delete all

--- src/script.js
+++ src/script.js
@@ -15,17 +15,29 @@ var taskApp = new Vue({
   methods: {
     addTask: function(e) {
       e.preventDefault();
-      if(this.tasks.name != '' && this.tasks.name != null && this.tasks.name != ' '){
-          this.tasks.push({
-            name: this.tasks.name,
-            done: false
-          });
-          this.tasks.name = '';
-          this.saveTask();
+      if (this.tasks.name != undefined){
+        if(this.tasks.name.split('')[0] != " " && this.tasks.name != ''){
+            this.tasks.push({
+              name: this.tasks.name
+            })
+            this.tasks.name = '';
+            this.saveTask();
+        }
+        else {
+          alert ('Неправильно введены данные!')
+        }
+      }
+      else {
+        alert ('Неправильно введены данные!')
       }
     },
     deleteTask: function(task) {
       this.tasks.splice(this.tasks.indexOf(task), 1)
+      this.saveTask();
+    },
+    deleteAll: function (e) {
+      e.preventDefault();
+      this.tasks = [];
       this.saveTask();
     },
     saveTask() {
